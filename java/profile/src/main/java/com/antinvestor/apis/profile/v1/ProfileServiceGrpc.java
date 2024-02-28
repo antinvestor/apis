049d71f502ceb62fda3ad193a31dacca2b01f072
Peter Bwire <bwire517@gmail.com>
2024-02-28 07:31:12 +0300

    bugfix settings client to use zoned datetime

--- java/profile/src/main/java/com/antinvestor/apis/profile/v1/ProfileServiceGrpc.java
+++ java/profile/src/main/java/com/antinvestor/apis/profile/v1/ProfileServiceGrpc.java
@@ -22,7 +22,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.61.1)",
+    value = "by gRPC proto compiler (version 1.62.2)",
     comments = "Source: profile/v1/profile.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class ProfileServiceGrpc {
