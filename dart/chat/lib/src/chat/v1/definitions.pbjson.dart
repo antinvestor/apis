ca7eeaa1bd49459480a61490df64b42c03d6d5b9
Peter Bwire <bwire517@gmail.com>
2026-01-06 17:23:10 +0300

    bugfix live updates to chat api service

--- dart/chat/lib/src/chat/v1/definitions.pbjson.dart
+++ dart/chat/lib/src/chat/v1/definitions.pbjson.dart
@@ -89,26 +89,25 @@ final $typed_data.Uint8List roomEventDescriptor = $convert.base64Decode(
 const AckEvent$json = {
   '1': 'AckEvent',
   '2': [
-    {'1': 'room_id', '3': 1, '4': 1, '5': 9, '8': {}, '9': 0, '10': 'roomId', '17': true},
+    {'1': 'room_id', '3': 1, '4': 1, '5': 9, '8': {}, '10': 'roomId'},
     {'1': 'event_id', '3': 2, '4': 3, '5': 9, '8': {}, '10': 'eventId'},
     {'1': 'subscription_id', '3': 3, '4': 1, '5': 9, '10': 'subscriptionId'},
     {'1': 'ack_at', '3': 4, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '10': 'ackAt'},
-    {'1': 'error', '3': 7, '4': 1, '5': 11, '6': '.common.v1.ErrorDetail', '9': 1, '10': 'error', '17': true},
+    {'1': 'error', '3': 7, '4': 1, '5': 11, '6': '.common.v1.ErrorDetail', '9': 0, '10': 'error', '17': true},
   ],
   '8': [
-    {'1': '_room_id'},
     {'1': '_error'},
   ],
 };
 
 /// Descriptor for `AckEvent`. Decode as a `google.protobuf.DescriptorProto`.
 final $typed_data.Uint8List ackEventDescriptor = $convert.base64Decode(
-    'CghBY2tFdmVudBI8Cgdyb29tX2lkGAEgASgJQh66SBvYAQFyFhADGCgyEFswLTlhLXpfLV17My'
-    'w0MH1IAFIGcm9vbUlkiAEBEj0KCGV2ZW50X2lkGAIgAygJQiK6SB+SARwIASIYchYQAxgoMhBb'
-    'MC05YS16Xy1dezMsNDB9UgdldmVudElkEicKD3N1YnNjcmlwdGlvbl9pZBgDIAEoCVIOc3Vic2'
-    'NyaXB0aW9uSWQSMQoGYWNrX2F0GAQgASgLMhouZ29vZ2xlLnByb3RvYnVmLlRpbWVzdGFtcFIF'
-    'YWNrQXQSMQoFZXJyb3IYByABKAsyFi5jb21tb24udjEuRXJyb3JEZXRhaWxIAVIFZXJyb3KIAQ'
-    'FCCgoIX3Jvb21faWRCCAoGX2Vycm9y');
+    'CghBY2tFdmVudBI3Cgdyb29tX2lkGAEgASgJQh66SBvYAQFyFhADGCgyEFswLTlhLXpfLV17My'
+    'w0MH1SBnJvb21JZBI9CghldmVudF9pZBgCIAMoCUIiukgfkgEcCAEiGHIWEAMYKDIQWzAtOWEt'
+    'el8tXXszLDQwfVIHZXZlbnRJZBInCg9zdWJzY3JpcHRpb25faWQYAyABKAlSDnN1YnNjcmlwdG'
+    'lvbklkEjEKBmFja19hdBgEIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBSBWFja0F0'
+    'EjEKBWVycm9yGAcgASgLMhYuY29tbW9uLnYxLkVycm9yRGV0YWlsSABSBWVycm9yiAEBQggKBl'
+    '9lcnJvcg==');
 
 @$core.Deprecated('Use receiptEventDescriptor instead')
 const ReceiptEvent$json = {
